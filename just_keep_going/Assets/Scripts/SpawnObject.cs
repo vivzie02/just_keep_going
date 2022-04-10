aab1ca9519d12938d425f09d1b7e61397d03bb5b
SirPoz <nicolas@pozdena.at>
2022-04-10 12:39:46 +0200

    Pixel Pallete Import
    Imported a Unity assets to design an customize rooms

--- just_keep_going/Assets/Scripts/SpawnObject.cs
+++ just_keep_going/Assets/Scripts/SpawnObject.cs
@@ -11,9 +11,31 @@ public class SpawnObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        setupRoom();
+    }
+
+    void setupRoom()
+    {
+
         int rand = Random.Range(0, objects.Length);
         Instantiate(objects[rand], transform.position, Quaternion.identity);
         Instantiate(levelHandler, transform.position, Quaternion.identity);
+    }
+
+    private void deleteRoom()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject levelHandlerInstance = GameObject.FindGameObjectWithTag("LevelHandler");
+        GameObject room = GameObject.FindGameObjectWithTag("Room");
+       Destroy(room);
+       Destroy(levelHandlerInstance);
+       Destroy(player);
+    }
+
+    public void restartRoom()
+    {
+        deleteRoom();
+        setupRoom();
     }
 
 }
